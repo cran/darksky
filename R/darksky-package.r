ac5389500ade2b09390abf0a7a05816cf4529c50
Bob Rudis <bob@rud.is>
2016-09-22 09:22:49 +0000

    version 1.0.0

---
+++ R/darksky-package.r
@@ -0,0 +1,16 @@
+#' Tools to Work with the Dark Sky API
+#'
+#' Programmatic access to the Dark Sky API <https://darksky.net/dev/docs>,
+#' which provides current or historical global weather conditions.
+#'
+#' @name darksky
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import dplyr
+#' @importFrom httr GET content stop_for_status headers
+#' @importFrom grid unit
+#' @importFrom gridExtra grid.arrange
+#' @import ggplot2
+#' @importFrom stats setNames time
+#' @import gtable
+NULL
